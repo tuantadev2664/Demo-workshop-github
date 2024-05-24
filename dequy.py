aa305a84eff618b9361b3e99b254c3e6f64cd372
Trương Anh Tuấn <134388467+tuantadev2664@users.noreply.github.com>
2024-05-24 19:49:17 +0700

    tuan

--- dequy.py
+++ dequy.py
@@ -1,11 +1,11 @@
-a = int(input("Enter num1: "))
+a = int(input("Enter num1: ")) 
 b = int(input("Enter num2: "))
 def gcd(a, b):
     if(b == 0):
         return a
     if(a > b):
         return gcd(b, a % b)
-    elif(a == b):
+    elif(a != b):
         return a
     else:
         return gcd(a, b % a)
