798a63ecf7587b7c097c9ed063d68406c8e7ac21
Trương Anh Tuấn <134388467+tuantadev2664@users.noreply.github.com>
2024-05-24 20:24:10 +0700

    Tuan (#5)
    * tuan
    
    * tuan ver2

--- dequy.py
+++ dequy.py
@@ -5,13 +5,8 @@ def gcd(a, b):
         return a
     if(a > b):
         return gcd(b, a % b)
-<<<<<<< HEAD
     elif(a == b):
         return abc
-=======
-    elif(a != b):
-        return a
->>>>>>> bdfe6c7c4ce7aa37c202612cce741e7b4359f489
     else:
         return gcd(a, b % a)
     
